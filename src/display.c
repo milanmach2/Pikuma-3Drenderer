f68de1a2387484dce8445f26aca03dc5a94fe12c
milanmach2 <milanmach2@gmail.com>
2023-11-09 21:06:09 +0100

    Initial commit

---
+++ src/display.c
@@ -0,0 +1,104 @@
+#include "display.h"
+
+
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+int window_width = 800;
+int window_height = 600;
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return false;
+	}
+	// Use SDL to query what is the fullscreen max. width and height
+
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
+	// Create a SDL Window
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		window_width,
+		window_height,
+		SDL_WINDOW_BORDERLESS
+		 );
+	if (!window) {
+		fprintf(stderr, "Error Creating SDL window.\n");
+		return false;
+	}
+	// todo:  Create a SDL renderer
+	// return true;
+	
+	// create a SDL renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+	fprintf(stderr, "Error Creating SDL renderer.\n");
+        return false;
+	}
+	//SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+	return true;
+}
+
+void draw_grid(void) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            if (x % 20 == 0 || y % 20 == 0) {
+                color_buffer[(window_width * y) + x] = 0xFFFFFFFF;
+            }
+        }
+    }
+}
+
+void draw_pixel(int x, int y, uint32_t color){
+    if (x >= 0 && x < window_width && y >= 0 && y < window_height) {
+        color_buffer[(window_width * y) + x] = color;
+    }
+    
+}
+
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+	for (int j = 0; j < width; j++) {
+            for (int k = 0; k < height; k++) {
+		int current_x = x + j;
+		int current_y = y + k;
+                draw_pixel(current_x, current_y, color);
+            }
+        }
+}
+
+void render_color_buffer(void) {
+	SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		(int)(window_width * sizeof(uint32_t))
+	);
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+	for (int y = 0; y < window_height; y++) {
+		for (int x = 0; x < window_width; x++) {
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+	
+}
+
+void destroy_window(void) {
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+}
+
---
+++ src/main.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "display.h"
+#include "vector.h"
+
+
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS]; // 9x9x9 cube
+vec2_t projected_points[N_POINTS];
+
+vec3_t camera_position = {.x = 0, .y = 0, .z = -5}; // 9x9x9 cube
+vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
+
+float fov_factor = 640;
+
+bool is_running = false;
+int previous_frame_time = 0;
+
+
+void setup(void) {
+	// Alloc the memory in bytes to hold the color buffer
+	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+	// Creating a SDL texture that is used to display the color
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+    
+    // start loading my array of vectors
+    // from -1 to 1 in this  9x9x9 cube
+    
+    int point_count = 0;
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25){
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z};
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
+}
+
+
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	
+	switch (event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+        		break;
+	}
+	
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Function that receives a 3D vector and returns a projected 2D point
+////////////////////////////////////////////////////////////////////////////////
+
+vec2_t project(vec3_t point) {
+    vec2_t projected_point = {
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z
+    };
+    return projected_point;
+}
+
+void update(void) {
+    
+    // Wait some time until the reach the target frame time in milliseconds
+    int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+
+    // Only delay execution if we are running too fast
+    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+        SDL_Delay(time_to_wait);
+    }
+    
+    previous_frame_time = SDL_GetTicks();
+    
+    cube_rotation.x += 0.01;
+    cube_rotation.y += 0.01;
+    cube_rotation.z += 0.01;
+    
+    for (int i = 0; i < N_POINTS; i++) {
+        vec3_t point = cube_points[i];
+        
+        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+        
+        // Translate the point away from the camera
+        transformed_point.z -= camera_position.z;
+        
+        // project the current point
+        vec2_t projected_point = project(transformed_point);
+        // save the projected point in the array of projected points
+        projected_points[i] = projected_point;
+        
+    }
+}
+
+void render(void) {
+
+	//draw_grid();
+    
+    
+    // loop all projected points and render them
+    for (int i = 0; i < N_POINTS; i++) {
+        vec2_t projected_point = projected_points[i];
+        draw_rect(
+                  projected_point.x + window_width / 2,
+                  projected_point.y + window_height / 2,
+                  4,
+                  4,
+                  0xFFFFFF00
+                  );
+    }
+    
+	render_color_buffer();	
+	clear_color_buffer(0xFF000000);
+
+	SDL_RenderPresent(renderer);
+	
+}
+
+
+int main(void) {
+	/* TODO: Create a SDL window */
+	
+	is_running = initialize_window();
+	
+	setup();
+    
+    
+    
+	while (is_running) {
+		process_input();
+		update();
+		render();
+	}
+    
+    destroy_window();
+	
+	return 0;	
+
+}
